a4652fc78d80a2155c7ff826b4942165bee41747
shiqing881215 <shiqing881215@gmail.com>
2018-10-18 13:05:03 -0700

    Initial working version : 127.0.0.1

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    // having this makes all routing works
+    historyApiFallback: true,
+    // where server should look for files
+    contentBase: './dist'
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+      // title : "test"
+    })
+  ]
+};
